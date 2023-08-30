5df699b2829c32b6fe0b756b64c728ef0f26937a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-08-30 19:12:45 +0100

    refactor: update type aBlogResponse Type status attribute

--- src/app/types/responseTypes.ts
+++ src/app/types/responseTypes.ts
@@ -2,7 +2,7 @@ import { Post, PostAttributes } from '../model/Post'
 import { ValidationError } from 'express-validator'
 
 export interface PostResponse {
-  status: number
+  status: boolean
   message: string
   data?: Post | PostAttributes[]
   errors?: ValidationError | ValidationError[]
