997ab4be9e4c70ecc2f92c32b0e3c51d6adfb77b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-08-31 22:09:08 +0100

    style: remove unused import

--- src/app/controllers/post.controller.ts
+++ src/app/controllers/post.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Response } from 'express'
 import { matchedData } from 'express-validator'
 import { Post } from '../model/post.model'
 import { PostResponse } from '../types/responseTypes'
