f0f6cb98f4487fd40aa7ca6c2b40b17ca187dd18
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-09-01 01:11:28 +0100

    test: setup database for tests

--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -3,25 +3,36 @@ import config from '../config'
 import { postModel } from './post.model'
 import { dbType } from '../types/dbType'
 
-const sequelize = new Sequelize(
-  config.DATABASE,
-  config.DB_USERNAME,
-  config.DB_PASSWORD,
-  {
+let sequelize: Sequelize
+
+if (process.env.NODE_ENV === 'test') {
+  sequelize = new Sequelize(process.env.TEST_DB_URL ?? '', {
     logging: false,
-    host: config.DB_HOST,
-    dialect: config.DB_DIALECT,
-    pool: {
-      min: config.DB_POOL_MIN,
-      max: config.DB_POOL_MAX,
-      idle: config.DB_POOL_IDLE,
-      acquire: config.DB_POOL_ACQUIRE
-    },
     define: {
-      freezeTableName: true // prevents Sequelize from auto-pluralization of model names
+      freezeTableName: true
     }
-  }
-)
+  })
+} else {
+  sequelize = new Sequelize(
+    config.DATABASE,
+    config.DB_USERNAME,
+    config.DB_PASSWORD,
+    {
+      logging: false,
+      host: config.DB_HOST,
+      dialect: config.DB_DIALECT,
+      pool: {
+        min: config.DB_POOL_MIN,
+        max: config.DB_POOL_MAX,
+        idle: config.DB_POOL_IDLE,
+        acquire: config.DB_POOL_ACQUIRE
+      },
+      define: {
+        freezeTableName: true // prevents Sequelize from auto-pluralization of model names
+      }
+    }
+  )
+}
 
 const db: dbType = { sequelize, post: postModel(sequelize) }
 
